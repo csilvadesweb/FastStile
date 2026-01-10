2fe2318e37a7efff075c85965146b8d3276e418e
C.Silva <csdesweb@gmail.com>
2026-01-10 16:50:40 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
-const CACHE_NAME = 'faststile-v1';
+const CACHE_NAME = 'faststile-v2';
 const ASSETS = [
-  '/FastStile/',
-  '/FastStile/index.html',
-  '/FastStile/style.css',
-  '/FastStile/script.js',
-  '/FastStile/manifest.json'
+    '/FastStile/',
+    '/FastStile/index.html',
+    '/FastStile/style.css',
+    '/FastStile/script.js',
+    '/FastStile/manifest.json'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
 });
