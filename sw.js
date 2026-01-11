b5e114448981439716e154b000ff837d99c8694b
C.Silva <csdesweb@gmail.com>
2026-01-10 21:43:08 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,36 @@
-const CACHE = "faststile-v1";
+const CACHE_NAME = "faststile-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "https://cdn.jsdelivr.net/npm/chart.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+];
 
+// Instalação
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./script.js"
-      ])
-    )
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Ativação e limpeza de cache antigo
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
+// Estratégia: Tenta rede, se falhar, usa cache
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
