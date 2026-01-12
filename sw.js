536a40000b55b7d68c64b1f084ff43688958f5b4
C.Silva <csdesweb@gmail.com>
2026-01-12 16:30:39 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
-/* Conteúdo de sw.js já enviado */
+const CACHE_NAME = "faststile-v2-cache";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./App.js",
+  "./script.js",
+  "./manifest.json",
+  "https://cdn.jsdelivr.net/npm/chart.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(resp => resp || fetch(event.request))
+    );
+});
