9129f0a318076f410d530e8dbf35a7e9591cd0df
C.Silva <csdesweb@gmail.com>
2026-01-13 08:20:09 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,17 @@
-const CACHE_NAME = "faststile-v3";
+const CACHE_NAME = 'faststile-v3';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./App.js",
-  "./script.js",
-  "./manifest.json"
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
